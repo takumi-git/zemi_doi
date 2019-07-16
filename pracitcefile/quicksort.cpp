aee5e4b86fc9bc2adba736c9e894e593b57dad99
takumi-git <taku.ssk@icloud.com>
2019-07-16 18:01:40 +0900

    quicksort

--- pracitcefile/quicksort.cpp
+++ pracitcefile/quicksort.cpp
@@ -1,39 +1,82 @@
-#include <iostream>
 #include <stdio.h>
-#define N 10
 
-static int a[N] = {6, 9, 12, 7, 15, 23, 2, 10, 4, 20};
-int main(){
- int i, j;
- int temp;
- int p =5;
+/* 値を入れ替える関数 */
+void swap(int *x, int *y)
+{
+  int temp; // 値を一時保存する変数
 
- /*for (i = 0; i < N;i++){
-  printf("%3d", a[i]);
- }
- printf("\n");*/
-
- while(i>j){
-  for (i = 0;a[i]>=a[p];i++){
-   for (j = N - 1; a[j] <= a[p];j--){
-    if(i<j && a[i] != a[j]){
-     temp = a[i];
-     a[i] = a[j];
-     a[j] = temp;
-     i++;
-     j--;
-    }else if(i==j|| a[i] == a[j]){
-     i++;
-     j--;
-    }
-  }
- }
- //最終表示
- for (j = 0; j < N; j++)
- {
-   printf("%3d", a[j]);
- }
- printf("\n");
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
-return 0;
+
+/***
+* pivotを決め、
+* 全データをpivotを境目に振り分け、
+* pivotの添え字を返す
+***/
+int partition(int array[], int left, int right)
+{
+  int i, j, pivot;
+  i = left;
+  j = right + 1;
+  pivot = left; // 先頭要素をpivotとする
+
+  do
+  {
+    do
+    {
+      i++;
+    } while (array[i] < array[pivot]);
+    do
+    {
+      j--;
+    } while (array[pivot] < array[j]);
+    // pivotより小さいものを左へ、大きいものを右へ
+    if (i < j)
+    {
+      swap(&array[i], &array[j]);
+    }
+  } while (i < j);
+
+  swap(&array[pivot], &array[j]); //pivotを更新
+
+  return j;
+}
+
+/* クイックソート */
+void quick_sort(int array[], int left, int right)
+{
+  int pivot;
+
+  if (left < right)
+  {
+    pivot = partition(array, left, right);//配列の右左の区切り
+    quick_sort(array, left, pivot - 1); // pivotを境に再帰的にクイックソート
+    quick_sort(array, pivot + 1, right);
+  }
+}
+
+int main(void)
+{
+  int array[10] = {2, 1, 8, 5, 4, 7, 9, 0, 6, 3};
+  int i;
+
+  printf("Before sort: ");
+  for (i = 0; i < 10; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+
+  quick_sort(array, 0, 9);
+
+  printf("After sort: ");
+  for (i = 0; i < 10; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+
+  return 0;
 }
